a8b20f7fd7880dba0bfc538b0e8b864ea9a336a3
GGX <1056937524@qq.com>
2025-05-28 21:07:45 +0800

    init

--- eslint.config.js
+++ eslint.config.js
@@ -1,61 +1,14 @@
-// eslint.config.mjs
-import pluginVue from 'eslint-plugin-vue'
-import {
-  defineConfigWithVueTs,
-  vueTsConfigs,
-  configureVueProject,
-} from '@vue/eslint-config-typescript'
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
 
-configureVueProject({
-  // Whether to parse TypeScript syntax in Vue templates.
-  // Defaults to `true`.
-  // Setting it to `false` could improve performance.
-  // But TypeScript syntax in Vue templates will then lead to syntax errors.
-  // Also, type-aware rules won't be applied to expressions in templates in that case.
-  tsSyntaxInTemplates: true,
 
-  // Optional: specify the script langs in `.vue` files
-  // Defaults to `['ts']`.
-  scriptLangs: [
-    'ts',
-
-    // [!DISCOURAGED]
-    // Include 'js' to allow plain `<script>` or `<script setup>` blocks.
-    // This might result-in false positive or negatives in some rules for `.vue` files.
-    // Note you also need to configure `allowJs: true` and `checkJs: true`
-    // in corresponding `tsconfig.json` files.
-    'js',
-
-    // [!STRONGLY DISCOURAGED]
-    // Include 'tsx' to allow `<script lang="tsx">` blocks.
-    // This would be in conflict with all type-aware rules.
-    'tsx',
-
-    // [!STRONGLY DISCOURAGED]
-    // Include 'jsx' to allow `<script lang="jsx">` blocks.
-    // This would be in conflict with all type-aware rules and may result in false positives.
-    'jsx',
-  ],
-
-  // <https://github.com/vuejs/eslint-plugin-vue/issues/1910#issuecomment-1819993961>
-  // Optional: the root directory to resolve the `.vue` files, defaults to `process.cwd()`.
-  // You may need to set this to the root directory of your project if you have a monorepo.
-  // This is useful when you allow any other languages than `ts` in `.vue` files.
-  // Our config helper would resolve and parse all the `.vue` files under `rootDir`,
-  // and only apply the loosened rules to the files that do need them.
-  rootDir: import.meta.dirname,
-})
-
-export default defineConfigWithVueTs(
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  tseslint.configs.recommended,
   pluginVue.configs["flat/essential"],
-
-  // We STRONGLY RECOMMEND you to start with `recommended` or `recommendedTypeChecked`.
-  // But if you are determined to configure all rules by yourself,
-  // you can start with `base`, and then turn on/off the rules you need.
-  vueTsConfigs.base,
-  {
-    ignores: [
-      'src-tauri/**'
-    ]
-  }
-)
+  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+]);
--- src/hooks/useConversation.ts
+++
@@ -1,6 +0,0 @@
-import { createGlobalState, useLocalStorage } from '@vueuse/core'
-
-// 使用自动同步的 storage
-const STORAGE_KEY = 'voice_assistant_history'
-
-export const useConversation = createGlobalState(() => useLocalStorage<null | string>(STORAGE_KEY, null));
